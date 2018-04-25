3ef41580429be8e0364364154041d6aa05de8c59
Robert Brown <robertlb4@gmail.com>
2018-04-24 20:24:16 -0700

    added loopback backend

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,15 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
  
 declare var google;
+let map: any;
+let infowindow: any;
+let options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
  
 @Component({
   selector: 'home-page',
@@ -11,31 +18,78 @@ declare var google;
 export class HomePage {
  
   @ViewChild('map') mapElement: ElementRef;
-  map: any;
  
   constructor(public navCtrl: NavController, public geolocation: Geolocation) {
  
   }
  
-  ionViewDidLoad(){
-    this.loadMap();
+  ionViewDidLoad() {
+    this.initMap();
   }
+
+  initMap() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      map = new google.maps.Map(this.mapElement.nativeElement, {
+        center: {lat: location.coords.latitude, lng: location.coords.longitude},
+        zoom: 15
+      });
+  
+      infowindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: {lat: location.coords.latitude, lng: location.coords.longitude},
+        radius: 1000,
+        type: ['resturaunt']
+      }, (results,status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            this.createMarker(results[i]);
+          }
+        }
+      });
+    }, (error) => {
+      console.log(error);
+    }, options);
+  }
+
+  createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeLoc
+    });
+  
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+  
+  
+  
+  
+  
+  
+  
+  // ionViewDidLoad(){
+  //   this.loadMap();
+  // }
  
-  loadMap(){
+  // loadMap(){
  
-    this.geolocation.getCurrentPosition().then(position => {
+  //   this.geolocation.getCurrentPosition().then(position => {
       
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //     let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
  
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
+  //     let mapOptions = {
+  //       center: latLng,
+  //       zoom: 15,
+  //       mapTypeId: google.maps.MapTypeId.ROADMAP
+  //     }
    
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  //     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
     
-    })
-    .catch(err => console.log(err));
-  }
+  //   })
+  //   .catch(err => console.log(err));
+  // }
 }
