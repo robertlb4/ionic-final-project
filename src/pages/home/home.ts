d29552dd588ec051eabc86761fa340596cbf13dd
Robert Brown <robertlb4@gmail.com>
2018-05-01 14:34:38 -0700

    cleaned up/deleted comments

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -24,94 +24,18 @@ export class HomePage {
   }
  
   ionViewDidLoad() {
-    // Promise.all([this._map.initMap(this.mapElement), this._map.getPlaces()])
-    // .then((res) => {
-    //   console.log(res);
-    // }, err=> console.log(err.message))
+  
     this.platform.ready().then(() => {
       this._map.initMap(this.mapElement)
       .then((res) => {
        return this._map.getPlaces()
      }, (err)=> console.log(err) )
       .then((res: any[]) => {
-        //res.forEach(marker => this._map.createMarker(marker))
+        res.forEach(marker => this._map.createMarker(marker))
       });
     })
      
   }
 
-  // initMap() {
-
-  //   navigator.geolocation.getCurrentPosition((location) => {
-  //     map = new google.maps.Map(this.mapElement.nativeElement, {
-  //       center: {lat: location.coords.latitude, lng: location.coords.longitude},
-  //       zoom: 14
-  //     });
-      
-  //     infowindow = new google.maps.InfoWindow();
-  //     let service = new google.maps.places.PlacesService(map);
-  //     service.nearbySearch({
-  //       location: {lat: location.coords.latitude, lng: location.coords.longitude},
-  //       radius: 2000,
-  //       type: ['restaurant', ]
-  //     }, (results,status) => {
-  //       if (status === google.maps.places.PlacesServiceStatus.OK) {
-  //         for (let i = 0; i < results.length; i++) {
-  //           this.createMarker(results[i]);
-  //         }
-  //       }
-  //     });
-  //   }, (error) => {
-  //     console.log(error);
-  //   }, options);
-  // }
-
-  // createMarker(place) {
-  //   let placeLoc = place.geometry.location;
-  //   let image = {
-  //     url: place.icon,
-  //     size: new google.maps.Size(71, 71),
-  //     origin: new google.maps.Point(0, 0),
-  //     anchor: new google.maps.Point(17, 34),
-  //     scaledSize: new google.maps.Size(25, 25)
-  //   };
-  //   let marker = new google.maps.Marker({
-  //     map: map,
-  //     position: placeLoc,
-  //     icon: image,
-  //   });
   
-  //   google.maps.event.addListener(marker, 'click', function() {
-  //     infowindow.setContent(place.name);
-  //     infowindow.open(map, this);
-  //   });
-  // }
-  
-  
-  
-  
-  
-  
-  
-  // ionViewDidLoad(){
-  //   this.loadMap();
-  // }
- 
-  // loadMap(){
- 
-  //   this.geolocation.getCurrentPosition().then(position => {
-      
-  //     let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
-  //     let mapOptions = {
-  //       center: latLng,
-  //       zoom: 15,
-  //       mapTypeId: google.maps.MapTypeId.ROADMAP
-  //     }
-   
-  //     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-    
-  //   })
-  //   .catch(err => console.log(err));
-  // }
 }
